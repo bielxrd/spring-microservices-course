ddc3ad42db982397c6dcfebe255c0cb558bd4119
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2025-10-26 00:26:45 -0300

    feat: config eureka server, config server, config client, eureka client

---
+++ task-service/src/main/java/br/com/service/tasks/HelloWorldController.java
@@ -0,0 +1,17 @@
+package br.com.service.tasks;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @Value("${message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
+    }
+}
