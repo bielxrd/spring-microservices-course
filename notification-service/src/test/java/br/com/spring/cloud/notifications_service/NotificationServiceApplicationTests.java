2cbaf821adc513d3efad30119184dedeb293b291
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2025-11-13 00:47:55 -0300

    feat: notification microservice

---
+++ notification-service/src/test/java/br/com/spring/cloud/notifications_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.cloud.notifications_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
